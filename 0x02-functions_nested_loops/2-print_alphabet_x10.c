d713f112a8f499243d8f86dd0953973c4883fb1f
Alfredchanja <alfredgachanja8@gmail.com>
2022-07-05 10:13:36 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int x;
+
+	x = 0;
+	while (x < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+}
