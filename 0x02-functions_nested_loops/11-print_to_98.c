cb2ae40cc4ca10fdf41b89d25e16099252441944
Alfredchanja <alfredgachanja8@gmail.com>
2022-07-05 12:55:49 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar('n');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			_putchar('n');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+	}
+	_putchar('\n');
+}
