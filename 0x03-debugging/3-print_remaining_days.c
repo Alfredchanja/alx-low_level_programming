d50fbc13dd94b0be2c01c4fc5ac79e1429dbd1d0
Alfredchanja <alfredgachanja8@gmail.com>
2022-07-07 15:17:35 -0700

    to finish tomorrow

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+3include "main.h"
+
+/**
+ * print_remaining_days - takes adateand prints how many days are
+ * les\ft in the year, taking leap years int account
+ * @month: month in number format
+ * @day : day of month
+ * @year:year
+ * Return: void
+ */
+
+void print_remaining_days(int moth, int day, int year)
+{
+}
