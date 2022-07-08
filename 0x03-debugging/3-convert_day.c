a9375c8f9c67c4cf94c9a4e509a77c307f7bf159
Alfredchanja <alfredgachanja8@gmail.com>
2022-07-08 04:59:00 -0700

    Leap year

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -10,7 +10,7 @@
 
 int convert_day(int month, int day)
 {
-	switvh (month)
+	switch (month)
 	{
 		case 2:
 			day = 31 + day;
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-3include "main.h"
+#include "main.h"
 
 /**
  * print_remaining_days - takes adateand prints how many days are
@@ -10,6 +10,30 @@
  * Return: void
  */
 
-void print_remaining_days(int moth, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	{
+		if (month >= 2 && day >=60)
+		{
+			day++;
+		}
+
+		printf("Day of the year: %i\n", day);
+		printf("Remaining days: %i\n", 366 - day);
+
+	}
+	else
+	{
+		if (month == 2 && day >= 60)
+		{
+			printf("Day of the year: %02d/%02d/%04d\n", month, day - 29, year);
+			printf("Remaining days: %i\n", 366 - day);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
