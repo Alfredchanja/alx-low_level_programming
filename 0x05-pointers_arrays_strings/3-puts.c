736c14ce017929df9628c8086d16d32424345b48
Alfredchanja <alfredgachanja8@gmail.com>
2022-07-11 12:58:38 -0700

    strcpy

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _puts - prints a string 
+ * _puts - prints a string
  * followed by a new line to stdouti
  * @str: pointer for the sring to print
  */
 void _puts(char *str)
 {
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
