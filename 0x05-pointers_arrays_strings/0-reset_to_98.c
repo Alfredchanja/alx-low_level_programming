111e421cca54c26dad6a5a79f9e2df6657926d67
Alfredchanja <alfredgachanja8@gmail.com>
2022-07-11 10:25:22 -0700

    betty again

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * reset_to_98 - updates the value it points to to 98
- * @n - pointer to store the address of n
- * Return - nothing
+ * @n: pointer to store the address of n
+ * Return: nothing
  */
 void reset_to_98(int *n)
 {
